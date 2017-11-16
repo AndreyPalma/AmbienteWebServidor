44173c2dc526afa27a5a768f776f4d23eda57580
AndreyPalma <andreypj98@gmail.com>
2017-11-15 23:06:48 -0600

    Examen 2

---
+++ Examen 2 - Andrey Palma Jimenez/Cuentas Bancarias/Banco.php
@@ -0,0 +1,31 @@
+<?php
+
+class Banco{
+	public $nombreBanco;
+	public $cuentas;
+	function __construct($nombreBanco){
+		$this->nombreBanco=$nombreBanco;
+		$this->cuentas=[];
+	}
+	
+	function agregarCuenta($cuentas){
+		$this->cuentas[]=$cuentas;
+	}
+	function buscar($buscaCuenta){
+		$existe=false;
+		foreach($this->cuentas as $cuenta){
+			if($cuenta->cuenta == $buscaCuenta){
+				echo "<b> La cuenta a nombre de $cuenta->cuenta si existe con un saldo de $cuenta->saldo colones </b>";
+				$existe = true;
+			}			
+		}
+		if($existe==false){
+			echo "<b> La cuenta $buscaCuenta no existe </b>";
+		}
+	}
+	
+}
+
+
+
+?>
